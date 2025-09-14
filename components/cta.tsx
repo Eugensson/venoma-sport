39a70e4f8febaff3cf0717f2c28bf269f61a687a
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-14 11:32:02 +0300

    Create Cta section

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,3 +1,19 @@
+import { FaArrowRight } from "react-icons/fa";
+
 export const Cta = () => {
-  return <section>Cta</section>;
+  return (
+    <section className="mt-24 py-20 bg-accent">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between">
+          <h2 className="h3 mb-6 xl:mb-0 text-center">
+            Founded with a passion for exellence
+          </h2>
+          <button type="button" className="btn btn-primary gap-2.5">
+            Browse our collection
+            <FaArrowRight />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
